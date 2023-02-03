72e9b60386d7303b81f2fd16842067d36534223f
Unicle-denisabuzan <denisa.buzan@unicle.life>
2023-02-03 17:30:18 +0100

    permission changed R file1

--- bin/script.R
+++
@@ -1,8 +0,0 @@
- FUN <- readRDS("function.rds");
- input = readRDS("input.rds");
- output = FUN(
-singleCell_data_input = input[[1]], savePath = input[[2]], tmpDirGC = input[[3]]
-);
- saveRDS(output, "output.rds")
-
-
